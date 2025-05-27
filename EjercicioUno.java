aee0258cd8c6187c6e2c3702098b0155763ce634
Isabella Pedraza Zapata <iszp99@gmail.com>
2025-05-27 14:45:49 -0500

    Se agrega comentario explicativo al ejercicio de colores

--- EjercicioUno.java
+++ EjercicioUno.java
@@ -2,10 +2,12 @@ public class EjercicioUno {
 
     public static void main(String[] args) {
 
-        System.out.print("\033[33m Isabella Pedraza");
-        System.out.print("\033[32m Isabella Pedraza");
-        System.out.print("\033[31m Isabella Pedraza");
-        System.out.print("\033[37m Isabella Pedraza");
+        /*Se realiza un programa que muestra mi nombre en pantalla en diferentes colores */
+
+        System.out.println("\033[33m Isabella Pedraza");
+        System.out.println("\033[32m Isabella Pedraza");
+        System.out.println("\033[31m Isabella Pedraza");
+        System.out.println("\033[37m Isabella Pedraza");
 
     }
 }
