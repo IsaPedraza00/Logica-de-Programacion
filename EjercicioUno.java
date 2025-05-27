08c29fa87464a54862a35b9a64012197c840da9c
Isabella Pedraza Zapata <iszp99@gmail.com>
2025-05-27 14:07:53 -0500

    Se sube primer ejercicio para aprender lógica de programación.En este caso, se practica cómo aplicar colores a las letras

---
+++ EjercicioUno.java
@@ -0,0 +1,11 @@
+public class EjercicioUno {
+
+    public static void main(String[] args) {
+
+        System.out.print("\033[33m Isabella Pedraza");
+        System.out.print("\033[32m Isabella Pedraza");
+        System.out.print("\033[31m Isabella Pedraza");
+        System.out.print("\033[37m Isabella Pedraza");
+
+    }
+}
