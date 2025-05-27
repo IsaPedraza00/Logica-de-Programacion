c0412cba918570539b131d35c9b5385e2a587862
Isabella Pedraza Zapata <iszp99@gmail.com>
2025-05-27 15:08:48 -0500

    Se agrega ejercicio número 2, el cual corresponde a la traducción de palabras distribuídas en columnas y líneas

---
+++ EjercicioDos.java
@@ -0,0 +1,19 @@
+public class EjercicioDos {
+
+    public static void main(String[] args) {
+
+        /*
+         * Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+         * correspondiente traducción al castellano. Las palabras deben estar
+         * distribuidas
+         * en dos columnas y alineadas a la izquierda.
+         */
+
+        System.err.println("  Español  |  Inglés  ");
+        System.out.println("Computador |Computer");
+        System.out.println("Estudiante |Alumno");
+        System.out.println("Gato       |Cat");
+
+    }
+
+}
--- EjercicioUno.java
+++ EjercicioUno.java
@@ -2,7 +2,7 @@ public class EjercicioUno {
 
     public static void main(String[] args) {
 
-        /*Se realiza un programa que muestra mi nombre en pantalla en diferentes colores */
+        /*1. Se realiza un programa que muestra mi nombre en pantalla en diferentes colores */
 
         System.out.println("\033[33m Isabella Pedraza");
         System.out.println("\033[32m Isabella Pedraza");
