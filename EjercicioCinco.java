895799690c186d0ac7a16cf354c1ac873d4b6a06
Isabella Pedraza Zapata <iszp99@gmail.com>
2025-05-29 15:24:08 -0500

    Se sube ejercicio 6, en el cual se calcula la propina de acuerdo al valor ingresado por el usuario

--- EjercicioCinco.java
+++ EjercicioCinco.java
@@ -4,32 +4,20 @@ public class EjercicioCinco {
 
     public static void main(String[] args) {
 
-        /*
-         * Escribe un programa que calcule el total de una factura a partir de la base
-         * imponible (precio sin IVA). La base imponible estará almacenada en una
-         * variable.
-         */
-
         Scanner scanner = new Scanner(System.in);
 
-        int cantidadProductos = 3;
-        double[] productos = new double[cantidadProductos];
+        System.out.println("Producto 1:");
+        double productoUno = scanner.nextDouble();
 
-        for (int i = 0; i < cantidadProductos; i++) {
-            System.out.print("Ingrese el valor del producto " + (i + 1) + ": ");
-            productos[i] = scanner.nextDouble();
-        }
+        System.out.println("Producto 2:");
+        double productoDos = scanner.nextDouble();
 
-        // Mostrar todos los productos
-        System.out.println("\nLista de productos ingresados:");
-        for (int i = 0; i < cantidadProductos; i++) {
-            System.out.println("Producto " + (i + 1) + ": " + productos[i]);
-        }
+        double valorSinImpuesto = productoUno + productoDos;
+        double valorConIva = valorSinImpuesto * 1.19; // Aplica IVA del 19%
 
-        double valorProductosSinIva = productos /  cantidadProductos;
-        System.out.println("Valor del producto sin iva es :" + valorProductosSinIva);
+        System.out.println("El valor de su compra sin IVA es: " + valorSinImpuesto);
+        System.out.println("El valor de su compra total con IVA es: " + valorConIva);
 
         scanner.close();
     }
-
 }
---
+++ EjercicioSeis.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class EjercicioSeis {
+    public static void main(String[] args) {
+        
+        /* Haz un programa en Java que le pida al usuario el valor de su comida y le calcule la propina (10%). Luego, debe mostrar:
+            -Valor de la comida
+            -Valor de la propina
+            -Total a pagar (comida + propina) */
+
+            Scanner scanner  = new Scanner(System.in);
+
+            System.out.println("Escriba el valor de la comida que compró");
+            double valorComida = scanner.nextDouble();
+
+            double propina =  valorComida * 0.10 ;
+            double comidaMasPropina = propina * valorComida;
+
+            System.out.println("El valor de su comida es de : " + valorComida);
+            System.out.println("Valor de la propina es de  : " + comidaMasPropina);
+            System.out.println("El total de valor a pagar (comida + propina )es de 30:  " +  comidaMasPropina);
+
+            scanner.close();
+    }
+}
